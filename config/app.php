5ee6a989822fb8ae4dae4ce690a507c88d18b819
Unknown <glrd.remi@gmail.com>
2018-09-24 10:51:30 +0200

    Add cors

--- config/app.php
+++ config/app.php
@@ -151,6 +151,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
